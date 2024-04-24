a5300ab0f8b3158c091e91f3af454bcef0a4fb71
Purizz <priskilasupit22@gmail.com>
2024-04-24 23:07:04 +0800

    contact and js animation

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var i = 0;
 var txt = "I’m Priskila, an Informatics Eng student focusing in 3D Design field"; /* Teks yang nmo tulis */
-var speed = 50; /* Seberapa cepat dpe animasi (satuan ms) */
+var speed = 35; /* Seberapa cepat dpe animasi (satuan ms) */
 
 function typeWriter() {
   if (i < txt.length) {
